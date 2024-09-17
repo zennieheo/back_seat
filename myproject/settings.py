de59819441b92f49b9f691fc66ba1653d546162f
jjdahye <jdh020213@naver.com>
2024-09-17 21:37:16 +0900

    secret key

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,7 +15,6 @@ SECRET_KEY = env('SECRET_KEY')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
