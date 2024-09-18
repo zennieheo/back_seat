2c017bbf2bcc24e67f83a1c9b21434e7e1b7c56c
zennieheo <163839027+zennieheo@users.noreply.github.com>
2024-09-18 13:36:19 +0900

    Update settings.py
    hide the secret key

--- myproject/settings.py
+++ myproject/settings.py
@@ -5,14 +5,7 @@ from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-SECRET_KEY = 'e870pqwhvr7@^9s%wlccu(uc!0**4ry(ff)grmx(f)5y6q&%5y'
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = [config('ALLOWED_HOSTS', default='127.0.0.1')]
 
