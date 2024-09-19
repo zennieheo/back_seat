cd68525a19479d72ec8853e3d44d33715bd4cee8
jjdahye <jdh020213@naver.com>
2024-09-19 19:12:17 +0900

    fix6

--- myproject/settings.py
+++ myproject/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bus',
         'USER': 'bus-admin',
-        'PASSWORD': '1234',
+        'PASSWORD': '04321',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
