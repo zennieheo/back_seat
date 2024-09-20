2e59fd7c38d3129e18fb8448513b9c16255e03ed
jjdahye <jdh020213@naver.com>
2024-09-20 20:01:16 +0900

    password

--- myproject/settings.py
+++ myproject/settings.py
@@ -34,7 +34,7 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             "hosts": [("bb.fsm4zb.ng.0001.apn2.cache.amazonaws.com:6379", 6379)], 
-            'PASSWORD': config('REDIS_PASSWORD', default=None),
+            'PASSWORD': config('REDIS_PASSWORD'),
         },
     },
 }
