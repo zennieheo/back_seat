00df4047c92f46a16d4db778e7e48f86dead0ef9
zennieheo <163839027+zennieheo@users.noreply.github.com>
2024-09-20 22:39:02 +0900

    Update asgi.py444444

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,17 +1,15 @@
-# myproject/asgi.py (수정해봄
 import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 django.setup()
-django_asgi_app = get_asgi_application()
-from django.core.asgi import get_asgi_application
 
+from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from seat.consumers import SeatConsumer
 from seat.routing import websocket_urlpatterns
 
-
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
