284b2c8d58065239eaf8f6eed5e906f1c595a1c3
jjdahye <jdh020213@naver.com>
2024-09-20 19:56:10 +0900

    re

--- seat/routing.py
+++ seat/routing.py
@@ -1,7 +1,7 @@
 # seat/routing.py
-from django.urls import path
+from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/seats/', consumers.SeatConsumer.as_asgi()),
+    re_path('ws/seats/', consumers.SeatConsumer.as_asgi()),
 ]
