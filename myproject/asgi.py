fa80fc92e337c1424c8ab444156acf89f9b6639d
zennieheo <163839027+zennieheo@users.noreply.github.com>
2024-09-21 00:11:58 +0900

    Update asgi.py6

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,4 +1,10 @@
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
+import django
+django.setup()
+
+from user.models import Users
 
 from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
@@ -8,7 +14,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 import seat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = ProtocolTypeRouter(
     {
